2dcc5d13b94d51466b9c1fb854c283a01d71b13f
Abid Hasan <abidh@hsenidmobile.com>
2026-04-20 20:48:43 +0600

    chore: setup gradle build files and dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "dev.abid.noaa.weather"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "noaa-weather-sdk-kotlin"
